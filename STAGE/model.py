199060aa44e6303fceb808b1f8244ef0290d1151
sli231 <72546891+sli231@users.noreply.github.com>
2023-11-26 23:25:03 +0800

    Add files via upload

--- STAGE/model.py
+++ STAGE/model.py
@@ -32,12 +32,15 @@ class Encoder(nn.Module):
         self.fc4 = nn.Linear(50, 10)
         self.fc4_bn = nn.BatchNorm1d(10)
         self.fc5 = nn.Linear(10, X_dim)
-    def forward(self, input):
+    def forward(self, input, relu):
         h1 = F.relu(self.fc1_bn(self.fc1(input)))
         h2 = F.relu(self.fc2_bn(self.fc2(h1)))
         h3 = F.relu(self.fc3_bn(self.fc3(h2)))
         h4 = F.relu(self.fc4_bn(self.fc4(h3)))
-        return F.relu(self.fc5(h4))
+        if relu:
+            return F.relu(self.fc5(h4))
+        else:
+            return self.fc5(h4)
 
 
 class Decoder(nn.Module):
@@ -52,12 +55,15 @@ class Decoder(nn.Module):
         self.fc9 = nn.Linear(500, 1000)
         self.fc9_bn = nn.BatchNorm1d(1000)
         self.fc10 = nn.Linear(1000, gene_number)
-    def forward(self, z):
+    def forward(self, z, relu):
         h6 = F.relu(self.fc6_bn(self.fc6(z)))
         h7 = F.relu(self.fc7_bn(self.fc7(h6)))
         h8 = F.relu(self.fc8_bn(self.fc8(h7)))
         h9 = F.relu(self.fc9_bn(self.fc9(h8)))
-        return F.relu(self.fc10(h9))
+        if relu:
+            return F.relu(self.fc10(h9))
+        else:
+            return self.fc10(h9)
 
 
 def STAGE(
@@ -78,7 +84,8 @@ def STAGE(
         w_w=0.1,
         w_l1=0.1,
         step_size=500,
-        gamma=1
+        gamma=1,
+        relu=True
 ):
     """ This functions outputs generated or recovered data.
 
@@ -101,7 +108,7 @@ def STAGE(
             w_l1: Weight of L1 loss in reconstruction loss. Default is 0.1.
             step_size: Step size for learning rate dampling. Default is 500.
             gamma: Learning rate dampling ratio. Default is 1.
-
+            relu: Whether the output layer of encoder and decoder activated by ReLU. Default is True.
         Return:
             adata_stage: Generated AnnData object when experiment = "generation"; Recovered AnnData object when experiment = "recovery";
                 Generated AnnData object in real sections when experiment = "3d_model"
@@ -178,11 +185,11 @@ def STAGE(
 
                 xdata, xlabel, = Variable(xdata.cuda()), Variable(xlabel.cuda())
 
-                latent = encoder(xdata)
+                latent = encoder(xdata, relu)
                 latent = latent.view(-1, X_dim)
                 xlabel = xlabel.float().cuda()
                 latent_loss = loss1(latent, xlabel) + w_w * sliced_wasserstein_distance(latent, xlabel, 1000, device='cuda')
-                xrecon = decoder(latent)
+                xrecon = decoder(latent, relu)
                 recon_loss = loss2(xrecon, xdata) + w_l1 * loss1(xrecon, xdata)
 
                 total_loss = latent_loss + w_recon * recon_loss
@@ -218,7 +225,7 @@ def STAGE(
         normed_fill_coor_df = torch.from_numpy(np.array(normed_fill_coor_df))
         normed_fill_coor_df = normed_fill_coor_df.to(torch.float32)
         normed_fill_coor_df = Variable(normed_fill_coor_df.cuda())
-        generate_profile = decoder(normed_fill_coor_df)
+        generate_profile = decoder(normed_fill_coor_df, relu)
         generate_profile = generate_profile.cpu().detach().numpy()
 
         adata_stage = sc.AnnData(generate_profile)
@@ -244,7 +251,7 @@ def STAGE(
         normed_fill_coor_df = torch.from_numpy(np.array(normed_fill_coor_df))
         normed_fill_coor_df = normed_fill_coor_df.to(torch.float32)
         normed_fill_coor_df = Variable(normed_fill_coor_df.cuda())
-        generate_profile = decoder(normed_fill_coor_df)
+        generate_profile = decoder(normed_fill_coor_df, relu)
         generate_profile = generate_profile.cpu().detach().numpy()
 
         adata_stage = sc.AnnData(generate_profile)
@@ -258,7 +265,7 @@ def STAGE(
         normed_new_coor_df = torch.from_numpy(np.array(normed_new_coor_df))
         normed_new_coor_df = normed_new_coor_df.to(torch.float32)
         normed_new_coor_df = Variable(normed_new_coor_df.cuda())
-        generate_profile = decoder(normed_new_coor_df)
+        generate_profile = decoder(normed_new_coor_df, relu)
         generate_profile = generate_profile.cpu().detach().numpy()
 
         adata_simu = sc.AnnData(generate_profile)
@@ -277,4 +284,132 @@ def STAGE(
         adata_all.write(save_path + "/all_data.h5ad")
 
         return adata_stage, adata_simu, adata_all
+
+def VGE(
+        adata,
+        save_path='./VGE_results',
+        down_ratio=0.5,
+        coord_sf=77,
+        train_epoch=2000,
+        seed=1234,
+        batch_size=512,
+        learning_rate=1e-3,
+        w_recon=0.1,
+        w_l1=0.1,
+        step_size=500,
+        gamma=1,
+        relu=True
+):
+    """ This functions outputs generated or recovered data.
+
+        Args:
+            adata: AnnData object storing original data. Raw data should to be normalized. Highly variable genes should be identified.
+            save_path: File path saving results including net and AnnData object.
+            down_ratio: Down-sampling ratio. Default is 0.5.
+            coord_sf: Size factor to scale spatial location. Default is 77.
+            train_epoch: Training epoch number. Default is 2000.
+            batch_size: Batch size. Default is 512.
+            learning_rate: Learning rate. Default is 1e-3.
+            w_recon: Weight of reconstruction loss in total loss. Default is 0.1.
+            w_l1: Weight of L1 loss in reconstruction loss. Default is 0.1.
+            step_size: Step size for learning rate dampling. Default is 500.
+            gamma: Learning rate dampling ratio. Default is 1.
+            relu: Whether the output layer of encoder and decoder activated by ReLU. Default is True.
+
+        Return:
+            adata_vge: Generated AnnData object when experiment = "generation"; Recovered AnnData object when experiment = "recovery";
+                Generated AnnData object in real sections when experiment = "3d_model"
+            adata_simu: Generated AnnData object in simulated sections. Available when experiment = "3d_model".
+            adata_sample: Down-sampled AnnData object. Available when experiment = "recovery".
+    """
+
+    # set random seed
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.benchmark = True
+
+    # Preparation
+    coor_df, fill_coor_df, sample_index, sample_barcode = recovery_coord(adata, down_ratio=down_ratio)
+    used_gene, normed_data, adata_sample = get_data(adata, experiment='recovery', sample_index=sample_index, sample_barcode=sample_barcode)
+
+    normed_coor_df = coor_df / coord_sf
+    X_dim = 2
+
+    transformed_dataset = MyDataset(normed_data=normed_data, coor_df=normed_coor_df, transform=transforms.Compose([ToTensor()]))
+    train_loader = DataLoader(transformed_dataset, batch_size=batch_size, shuffle=True, num_workers=0, drop_last=False)
+
+    # Training process
+    if not os.path.isdir(save_path):
+        os.mkdir(save_path)
+
+    gene_number = normed_data.shape[0]
+    decoder = Decoder(gene_number, X_dim)
+
+    decoder.train()
+
+    if torch.cuda.is_available():
+        decoder = decoder.cuda()
+
+    dec_optim = optim.Adam(decoder.parameters(), lr=learning_rate)
+
+    dec_sche = optim.lr_scheduler.StepLR(dec_optim, step_size=step_size, gamma=gamma)
+
+    with tqdm(range(train_epoch), total=train_epoch, desc='Epochs') as epoch:
+        for j in epoch:
+
+            train_loss = []
+            train_re_loss = []
+
+            for xdata, xlabel in train_loader:
+                xdata = xdata.to(torch.float32)
+                xlabel = xlabel.to(torch.float32)
+
+                dec_optim.zero_grad()
+
+                xdata, xlabel, = Variable(xdata.cuda()), Variable(xlabel.cuda())
+
+                xrecon = decoder(xlabel, relu)
+                recon_loss = loss2(xrecon, xdata) + w_l1 * loss1(xrecon, xdata)
+
+                total_loss = w_recon * recon_loss
+
+                total_loss.backward()
+
+                dec_optim.step()
+
+                dec_sche.step()
+
+                train_re_loss.append(recon_loss.item())
+                train_loss.append(total_loss.item())
+
+            epoch_info = 'recon_loss: %.5f, total_loss: %.5f' % \
+                         (torch.mean(torch.FloatTensor(train_re_loss)),
+                          torch.mean(torch.FloatTensor(train_loss)))
+            epoch.set_postfix_str(epoch_info)
+
+    torch.save(decoder, save_path+'/decoder.pth')
+
+    decoder.eval()
+
+    # Get recovered data
+    normed_fill_coor_df = fill_coor_df.copy()
+    normed_fill_coor_df = normed_fill_coor_df / coord_sf
+    normed_fill_coor_df = torch.from_numpy(np.array(normed_fill_coor_df))
+    normed_fill_coor_df = normed_fill_coor_df.to(torch.float32)
+    normed_fill_coor_df = Variable(normed_fill_coor_df.cuda())
+    generate_profile = decoder(normed_fill_coor_df, relu)
+    generate_profile = generate_profile.cpu().detach().numpy()
+
+    adata_vge = sc.AnnData(generate_profile)
+    adata_vge.obsm["coord"] = fill_coor_df.to_numpy()
+    adata_vge.var.index = used_gene
+
+    adata.write(save_path + '/original_data.h5ad')
+
+    adata_sample.write(save_path + '/sampled_data.h5ad')
+    adata_vge.obs = adata.obs
+    adata_vge.write(save_path + '/recovered_data.h5ad')
+    return adata_sample, adata_vge
 
